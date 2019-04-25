f28a631ebe39bc4fbb6c079dff74dbbfbfb76594
Ibrahim Fouad <iibrahim.fouad@outlook.com>
2019-04-25 09:39:35 +0200

    Add Cars controller for testing get specific data.

---
+++ SmartPlate.API/Controllers/CarsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace SmartPlate.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        [HttpGet("id/1")]
+        [Obsolete("For testing only.")]
+        public IActionResult GetCar()
+        {
+            return Ok(new
+            {
+                id = 1,
+                plateNumber = "ع و ص 123",
+                shaseehNumber = 1,
+                motorNumber = 2,
+                color = "red",
+                model = 2019,
+                marka = "KIA",
+                type = "SERATO",
+                vechileType = "prive",
+                fuel = 95,
+                capacity = 200,
+                passengers = 4,
+                endDate = DateTime.Now.AddYears(10),
+                startDate = DateTime.Now
+            });
+        }
+    }
+}
